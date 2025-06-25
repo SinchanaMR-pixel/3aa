5050f3bbe030de75a7ab2b1755d09edc70e92f4d
SinchanaMR-pixel <sinchanamr13@gmail.com>
2025-06-25 14:57:16 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,12 @@
+# Assign initial values
+a = 5
+b = 2
+
+# Print before swapping
+print("Before swapping: a =", a, ", b =", b)
+
+# Swap the values
+a, b = b, a
+
+# Print after swapping
+print("After swapping: a =", a, ", b =", b)
